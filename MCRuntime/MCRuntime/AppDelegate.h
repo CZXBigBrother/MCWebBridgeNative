821de19e80b02d9a2464c27a3b9492761dd2b4a3
“marcoChen” <mchenxing@hotmail.com>
2016-12-01 18:22:12 +0800

    Pod出bug了 悲剧

---
+++ MCRuntime/MCRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCRuntime
+//
+//  Created by marco chen on 2016/12/1.
+//  Copyright © 2016年 marco chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
